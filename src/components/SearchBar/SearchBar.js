84eff7b20f32c590f7e4d3c991a4f50d21479bba
Margo <margo.bozhenova@gmail.com>
2020-03-07 16:18:50 +0300

    Add Yelp search simulation

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,9 +7,9 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'term': '',
-      'location': '',
-      'sortBy': 'best_match'
+      term: '',
+      location: '',
+      sortBy: 'best_match'
     };
 
     this.sortByOptions = {
@@ -17,6 +17,10 @@ class SearchBar extends React.Component {
       'Highest Rated': 'rating',
       'Most Reviewed': 'review_count'
     };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   getSortByClass(sortByOption) {
@@ -31,6 +35,23 @@ class SearchBar extends React.Component {
     this.setState({
       sortBy: sortByOption
     })
+  }
+
+  handleTermChange(e) {
+    this.setState({
+      term: e.target.value
+    });
+  }
+
+  handleLocationChange(e) {
+    this.setState({
+      location: e.target.value
+    });
+  }
+
+  handleSearch(e) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    e.preventDefault();
   }
 
   renderSortByOptions() {
@@ -49,11 +70,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-          <a href="ya.ru">Let's Go</a>
+          <a onClick={this.handleSearch} href="ya.ru">Let's Go</a>
         </div>
       </div>
     )

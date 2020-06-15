cc7368c4f08aaf880b9a62ba4173b60913e1dec0
Margo <margo.bozhenova@gmail.com>
2020-06-15 04:05:06 +0300

    Updates

---
+++ precache-manifest.2e7135ccb6d2ff27c573a2f7eb4e9163.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd619fc07f70d98e7969a4bd1525d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5ed4e0eabd5dd1f9b9",
+    "url": "/static/css/main.22322719.chunk.css"
+  },
+  {
+    "revision": "864eb8b8ebd1b5a6eec4",
+    "url": "/static/js/2.04353ae6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.04353ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5ed4e0eabd5dd1f9b9",
+    "url": "/static/js/main.104937de.chunk.js"
+  },
+  {
+    "revision": "c86bda5beec795370e44",
+    "url": "/static/js/runtime-main.013b9dec.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
